7d56656e8372978ca70ea4d4d90297cca9284589
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-20 19:21:14 +0200

    creata tabella  trains

---
+++ database/migrations/2022_09_20_170238_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 20);
+            $table->string('stazione_partenza', 60);
+            $table->string('stazione_arrivo', 60);
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->string('codice_treno', 20);
+            $table->smallInteger('numero_carrozze')->unsigned();
+            $table->string('stato_treno', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
